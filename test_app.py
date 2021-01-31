c4c90bcd550b9169bbdf16dd4402016151bfb031
Robert Foltz <rfoltz512@gmail.com>
2021-01-30 20:36:09 -0500

    Forgot a colon....

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from flask import request, jsonify
 from app import app
 
 def test_predict():
-    with app.test_client() as c
+    with app.test_client() as c:
         resp = c.post('/predict')
         json_data = resp.get_json()
         assert json_data['prediction'] == [20.35373177134412]
