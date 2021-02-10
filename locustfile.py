e39f1158dca108fd0293fc77887083a7643992da
Robert Foltz <rfoltz512@gmail.com>
2021-02-09 20:40:56 -0500

    Adding a locust file for load testing

---
+++ locustfile.py
@@ -0,0 +1,27 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+
+    @task
+    def on_start(self):
+        self.client.post("/predict", json={
+   "CHAS":{
+      "0":0
+   },
+   "RM":{
+      "0":6.575
+   },
+   "TAX":{
+      "0":296.0
+   },
+   "PTRATIO":{
+      "0":15.3
+   },
+   "B":{
+      "0":396.9
+   },
+   "LSTAT":{
+      "0":4.98
+   }
+})
